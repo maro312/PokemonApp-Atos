b5634653624a6d6224912b848547984e6b4f88ea
maro312 <marawan.ah.ab@gmail.com>
2026-08-03 16:27:29 +0300

    Configure Swagger authentication JWT support

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,14 @@
 
 using Application.Extinsions;
+using Application.Options;
 using infrastructure.Data.Seeding;
 using infrastructure.DbContexts;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using System.Text;
 
 namespace API
 {
@@ -11,20 +17,64 @@ namespace API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
+            
+            builder.Configuration.AddJsonFile("secrets.json", optional: true, reloadOnChange: true);
 
             builder.Services.AddControllers();
             builder.Services.AddTransient<Seed>();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme."
+                });
+                options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecuritySchemeReference("Bearer", null, null),
+                        new List<string>()
+                    }
+                });
+            });
 
             builder.Services.AddDbContext<PokemonDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            builder.Services.Configure<JwtConfiguration>(builder.Configuration.GetSection(nameof(JwtConfiguration)));
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(jwt =>
+            {
+                var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JwtConfiguration:Secret").Value);
+                jwt.SaveToken = true;
+                jwt.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false, // For dev only
+                    ValidateAudience = false, // For dev only
+                    RequireExpirationTime = false, // For dev only
+                    ValidateLifetime = true
+                };
+                
+            });
+
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<PokemonDbContext>()
+                .AddDefaultTokenProviders();
+             
             builder.Services.AddPokemonServices(builder.Configuration);
 
             var app = builder.Build();
@@ -43,7 +93,6 @@ namespace API
                 }
             }
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -52,6 +101,7 @@ namespace API
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
@@ -61,3 +111,4 @@ namespace API
         }
     }
 }
+
